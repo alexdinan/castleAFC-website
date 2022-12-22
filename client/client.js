eef49a2781e99896f723e7b6c6183b5023409cf3
Alex Dinan <vwzn63@durham.ac.uk>
2022-12-22 16:24:39 +0000

    finalised teams get client side code

--- client/client.js
+++ client/client.js
@@ -10,7 +10,6 @@ getTeamsBtn.addEventListener('click' , listTeams);
 
 
 
-
 async function listTeams(){
     try{
         //send get requets to server and parse response
@@ -19,17 +18,17 @@ async function listTeams(){
         //add html list element for each team
         let teamList = "";
         for (const team of teamNames){
-            teamList += `<li class="team-list-item list-group-item"><span class="text-danger">${team}</span></li>`;
+            teamList += `<li class="team-list-item list-group-item"><span class="text-dark">${team}</span></li>`;
         }
         //add to DOM
         document.getElementById("teamHeading").innerHTML = "Teams:";
+        document.getElementById("teamSmallText").innerHTML = "click items below to load team details!";
         document.getElementById("teamNameList").innerHTML = teamList;
        
         //add event listeners
         for(const li of document.querySelectorAll(".team-list-item")){
             li.addEventListener("click", (event) => getTeamDetails(event.target.textContent));
         }
-
     }catch(error){
         //add 404 handling
         alert(error);
@@ -42,7 +41,18 @@ async function getTeamDetails(teamName){
     try{
         //send get request + parse response
         const details = JSON.parse(await (await fetch(endPointRoot + `teaminfo?name=${teamName}`)).text());
-        
+
+        //add title + image to header
+        const header = document.getElementById("cardHeader");
+        header.innerHTML=`<h1 class="card-title mt-3 mb-3 mx-auto my-auto">${details.name}</h1>`;
+
+        //add playstyle information
+        let body = "<hr>";
+        for(const [key,value] of Object.entries(details.playStyle)){
+            body += `<div class="d-flex flex-row"><h6>${key}:    <span class="text-primary">${value}</span></div><hr>`;
+        }
+        document.getElementById("cardBody").innerHTML = body;
+
 
     }catch(error){
         alert(error);
